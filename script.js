14840d06c6875e70425a608d6a3e5486f08f64d8
Nichollas RG <nichollascavalcanti@hotmail.co.uk>
2023-12-18 21:58:51 +0000

    fixed the fetching of dataset and made the bars showup

--- script.js
+++ script.js
@@ -7,51 +7,58 @@ fetch("json/GDP-data.json")
     return response.json();
   })
   .then((obj) => {
-    dataset = obj;
-    console.log(dataset.data[8][1]);
+    dataset = obj.data;
+
+    const w = 800;
+    const h = 400;
+    const padding = 60;
+    console.log(dataset);
+    const xScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(dataset, (d) => d[1])])
+      .range([padding, w - padding]);
+
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, d3.max(dataset, (d) => d[1])])
+      .range([h - padding, padding]);
+
+    const svg = d3
+      .select("body")
+      .append("svg")
+      .attr("height", h + "px")
+      .attr("width", w + "px");
+
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale);
+
+    svg
+      .append("g")
+      .attr("transform", "translate(0, " + (h - padding) + ")")
+      .call(xAxis)
+      .attr("id", "x-axis");
+
+    svg
+      .append("g")
+      .attr("transform", "translate(" + padding + ", 0)")
+      .call(yAxis)
+      .attr("id", "y-axis");
+
+    // Creating bars
+
+    svg
+      .selectAll(".bar")
+      .data(dataset)
+      .enter()
+      .append("rect")
+      .attr("class", "bar")
+      .attr("x", (d) => xScale(d[1]))
+      .attr("y", (d) => yScale(d[1]))
+      .attr("width", 10)
+      .attr("height", (d) => h - padding - yScale(d[1]));
   })
   .catch((e) => {
     console.log("something's off");
     console.log(e);
   });
 //-----------------
-const w = 800;
-const h = 400;
-const padding = 60;
-const xScale = d3
-  .scaleLinear()
-  .domain([0, d3.max(dataset, (d) => dataset.data[d][1])])
-  .range([padding, w - padding]);
-const yScale = d3
-  .scaleLinear()
-  .domain([0, d3.max(dataset, (d) => dataset.data[d][1])])
-  .range([h - padding, padding]);
-
-const svg = d3
-  .select("body")
-  .append("svg")
-  .attr("height", h + "px")
-  .attr("width", w + "px");
-
-const xAxis = d3.axisBottom(xScale);
-const yAxis = d3.axisLeft(yScale);
-
-svg
-  .append("g")
-  .attr("transform", "translate(0, " + (h - padding) + ")")
-  .call(xAxis)
-  .attr("id", "x-axis");
-
-svg
-  .append("g")
-  .attr("transform", "translate(" + padding + ", 0)")
-  .call(yAxis)
-  .attr("id", "y-axis");
-
-d3.select("svg")
-  .selectAll("rect")
-  .data(dataset.data[d][1])
-  .enter()
-  .append("rect")
-  .attr("class", "bar")
-  .style("height", (d) => d + "px");
